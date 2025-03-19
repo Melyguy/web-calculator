5489d5083758956cb8674be3cd1be435d810a182
Odin-Johan <nidojohan@gmail.com>
2025-03-19 14:11:07 +0100

    build works

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,22 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
+  swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
